7c82162f5609ec95781c6efdaa5dbc0e9bdd8fc3
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-09-04 19:31:24 +0800

    finish visualization test

--- autoencoder_test.cpp
+++ autoencoder_test.cpp
@@ -39,27 +39,44 @@ EMat read_img(std::string const &file_name,
 void autoencoder_test()
 {
     cv::AutoBuffer<int> hidden_size(1);
-    hidden_size[0] = 32;
+    hidden_size[0] = 25;
 
+    //in this case, mini-batch is hard to reach ideal
+    //minimum, if the results are not good, train it again
+    //by reuse the trained layer until the results are
+    //reasonable. Or train the example with full batch size
     AutoEncoder ae(hidden_size);
-    ae.read("autoencoder_test_2.xml");
-    ae.set_reuse_layer(true);
+    /*{
+        std::ifstream in("autoencoder_test_2.xml");
+        if(in.is_open()){
+            ae.read("autoencoder_test_2.xml");
+        }
+    }*/
     ae.set_batch_size(400);
     ae.set_beta(3.0);
     ae.set_lambda(0.0001);
-    ae.set_learning_rate(0.1);
-    ae.set_max_iter(80000);
+    ae.set_learning_rate(4);
+    ae.set_max_iter(1000);
     ae.set_sparse(0.01);
-    ae.set_epsillon(1e-6);
+    ae.set_epsillon(1e-9);
+    {
+        auto const Train = read_img("image.txt", 64, 10000);
+        ae.train(Train);
+        ae.set_reuse_layer(true);
+        ae.set_batch_size(10000);
+        ae.set_learning_rate(3.8);
+        ae.set_max_iter(7000);
+        ae.train(Train);
+    }
+    ae.write("autoencoder_test_2.xml");
 
-    ae.train(read_img("image.txt", 64, 10000));
-    ae.write("autoencoder_test_2.xml");//*/
-
-    auto &layer = ae.get_layer()[0];
-    EMat const W1 = layer.w1_.transpose();
-    cv::Mat img = ocv::eigen::eigen2cv_ref(W1);
-    auto img_vision = visualize_network(img);
-    cv::resize(img_vision, img_vision, {}, 4, 4);
-    cv::imshow("", img_vision);
-    cv::waitKey();//*/
+    for(size_t i = 0; i != hidden_size.size(); ++i){
+        auto &layer = ae.get_layer()[i];
+        EMat const W1 = layer.w1_.transpose();
+        cv::Mat img = ocv::eigen::eigen2cv_ref(W1);
+        auto img_vision = visualize_network(img);
+        cv::resize(img_vision, img_vision, {}, 4, 4);
+        cv::imshow("", img_vision);
+        cv::waitKey();
+    }
 }
