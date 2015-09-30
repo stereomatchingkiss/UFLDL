74865183d02ccb8d88b810b1c2da2ea6fd9174e5
stereomatchingkiss <stereomatchingkiss@gmail.com>
2015-09-30 17:11:36 +0800

    remove header

--- main.cpp
+++ main.cpp
@@ -1,11 +1,11 @@
 #include "autoencoder_test.hpp"
-#include "self_taught_learning.hpp"
+//#include "self_taught_learning.hpp"
 #include "softmax_test.hpp"
 
 int main()
 {    
-    //autoencoder_test();
-    self_taught_learning();
+    autoencoder_test();
+    //self_taught_learning();
     //softmax_test();
 
     return 0;
